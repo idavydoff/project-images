7c58df93ba71d2a81bc24cedb94bc86c4fba08da
Classic <Ivan2002dav@gmail.com>
2019-12-21 13:38:49 +0300

    Row fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Navigation from "./components/navigation";
 
 const app = () => {
     return (
-        <div className="row">
+        <div className="row m-0">
             <div className="col-lg-10 offset-lg-1 p-0 pl-2 pr-2">
                 <div className="row m-4">
                     <div className="col-lg-8">
